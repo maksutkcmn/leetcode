82fe102a3a11eef03e8a5e6256162b1579b84c9f
Maksut Kocaman <makokcmn@gmail.com>
2025-12-14 15:54:32 +0300

    optimization

--- RegularExpressionMatching.cs
+++ RegularExpressionMatching.cs
@@ -1,25 +1,43 @@
 public class Solution {
 
+    //For optimization (like cache)
+    Dictionary<(int, int), bool> memoization;
+
     public bool Helper(string s, string p, int sIndex, int pIndex)
     {
+        if (memoization.ContainsKey((sIndex, pIndex)))
+            return memoization[sIndex, pIndex];
+
         if (pIndex == p.Length)
-            return sIndex == s.Length;
+        {
+            bool result = sIndex == s.Length; 
+            memoization[(sIndex, pIndex)] = result;
+            return result;
+        }
 
         bool firstMach = (sIndex < s.Length && (s[sIndex] == p[pIndex] || p[pIndex] == '.'));
 
+        bool answer;
+
         if (pIndex + 1 < p.Length && p[pIndex + 1] == '*')
         {
-            return Helper(s, p, sIndex, pIndex + 2) ||
+            answer = Helper(s, p, sIndex, pIndex + 2) ||
                 (firstMach && Helper(s, p, sIndex + 1, pIndex));
         }
+        else
+        {
+            answer = firstMach && Helper(s, p, sIndex + 1, pIndex + 1);            
+        }
         
-        return firstMach && Helper(s, p, sIndex + 1, pIndex + 1);
+        memoization[(sIndex, pIndex)] = answer;
+
+        return answer;
     }
 
     public bool IsMatch(string s, string p) {
         if (string.IsNullOrEmpty(p))
             return string.IsNullOrEmpty(s);
-
+        memoization = new Dictionary<(int, int), bool>();
         return Helper(s, p, 0, 0);
     }
 }
